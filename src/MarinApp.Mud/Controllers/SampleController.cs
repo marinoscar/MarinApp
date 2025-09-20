9527233161b83c33353f603e117d639ff12ac74d
Oscar Marin <oscar@marin.cr>
2025-09-19 21:18:54 -0500

    Add SampleController with basic GET endpoint
    Introduces SampleController to the project, providing a simple GET endpoint that returns a greeting message. This serves as a starting point for API development.

---
+++ src/MarinApp.Mud/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarinApp.Mud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from controller!");
+        }
+    }
+}
